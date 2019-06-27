c2ee570d6decb699d5f4f504310a96148aca97df
WHQ <2283149823@qq.com>
2019-06-28 01:30:07 +0800

    add

--- gisproject/src/components/page11.vue
+++ gisproject/src/components/page11.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-       <div>上海文化产业分布<a href=""  target="view_window">查看地图</a></div>
-
+       <h1>上海文化产业分布<a href="http://www.arcgis.com/home/webmap/viewer.html?url=https%3A%2F%2Flocalhost%3A6443%2Farcgis%2Frest%2Fservices%2Ftest1%2FMapServer&source=sd"  target="view_window">查看地图</a></h1>
+        <img src="../../static/image/比例.png" style="height: 600px;width: 1000px">
+        <br>
+        <hr><br><br>
+        <img src="../../static/image/比例地图.jpg" style="height: 700px;width: 1000px">
     </div>
 </template>
 
