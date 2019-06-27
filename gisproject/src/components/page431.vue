6d3cd035edc4f2a6e95eda7a9337219545bfd92f
ymir1226 <44184368+ymir1226@users.noreply.github.com>
2019-06-28 02:03:05 +0800

    new

--- gisproject/src/components/page431.vue
+++ gisproject/src/components/page431.vue
@@ -4,19 +4,21 @@
     <h2>环同济经济圈</h2>
     <span>环同济经济圈，即“环同济研发设计服务特色产业基地”，重点发展创意和设计产业，国际工程咨询服务业，新能源新材料和环保科技产业三大密集群。</span>
       <el-divider></el-divider>
-    <h2>核心区</h2>
+    <h3>核心区</h3>
     <span>该产业基地的“核心圈”以同济大学四平路校区为核心，包括密云路、中山北二路、江浦路、控江路、大连路围合组成的区域，面积约2.6平方公里。</span>
-    <el-divider></el-divider>
-    <h2>扩展区</h2>
+    <!-- <el-divider></el-divider> -->
+    <h3>扩展区</h3>
     <span>环同济经济圈扩展区以曲阳路、大连西路-大连路、周家嘴路、黄兴路、邯郸路围合组成，面积约 10 平方公里。</span>
     <span>在此基础上进一步向外扩展，形成四个“辐射点”，包括新江湾城辐射点、森林公园辐射点、黄兴公园辐射点、滨江辐射点。</span>
     <br><br>
     <div align="center">
-          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+          <el-button type="primary" icon="el-icon-search">
+               <a class="a-style" @click="linkDownload('https://www.baidu.com')">查看地图</a>
+              </el-button>
     </div>
     <br><br>
   <el-divider></el-divider>
-    <h2>环同济经济圈文化产业</h2>
+    <h3>环同济经济圈文化产业</h3>
     <el-row>
   <el-col :span="8">
     <el-card :body-style="{ padding: '0px' }">
@@ -29,25 +31,73 @@
     </el-card>
   </el-col>
 </el-row>
-    <div id="viewDivEcoCircle"></div>
-    <span>环同济经济圈文化产业数量为522，环同济经济扩展区文化产业数量为861，环同济经济圈辐射点共有2277家文化产业。</span>
-    <span>杨浦区的文化产业数量为3317，环同济经济圈文化产业数量为522，因此环同济经济圈内的文化产业占杨浦区文化产业比例为15.737%</span>
-    <span>至“十一五”末，杨浦区文化创意产业共有企业3237家，从业人员5.48万人；实现总产出199.61亿元，同比增长17.9%，与2004年（经济普查年）相比，年均增长18.9%，占全市比重5%；实现增加值64.88亿元，同比增长15.7%，占杨浦生产总值的比重为7.3%（扣除烟草业后占比13.5%），与上年基本持平，占全市文化创意产业比重为3.9%。环同济经济圈的文化产业的经济规模占杨浦区文化产业经济规模75.147%。</span>
-    <div id="mainDivEcoCircle" style="width: 100%;height:500px;"></div>
-    <span>下图所示的根据三次经济普查数据“环同济”产业结构的变化</span>
-    <div id="industryStruc" style="width: 100%;height:500px;"></div>
-    <span>因此参照《国民经济行业分类》目录，结合该区域的产业发展态势，可以把环同济知识经济圈文化创意产业划分为九大类。这九大类分别是：文化艺术类，信息服务类，产品设计类，建筑景观设计类，咨询策划类，文化用品、设备及相关文化产品的生产类，文化用品、设备及相关文化产品的销售类，教育、卫生、体育事业类，以及其他类。</span>
-    <span>其中重点发展的产业有四类（有先后次序）:</span>
-    <span>1.建筑景观设计类，具体包括建筑设计业、城市规划设计业、室内设计业、城市绿化设计业。</span>
-    <span>2.产品设计类，重点是工业设计业</span>
-    <span>3.文化艺术类，包括文化艺术活动业、出版业等。</span>
-    <span style="margin-bottom: 70px;">4.信息服务类，重点是互联网信息服务业。</span>
+ 
 <el-divider></el-divider>
-    <h2>文化产业企业</h2>
+    <h3>文化产业企业</h3>
+      <el-card>
+      <el-row>
+  <el-col :span="12"><div class="grid-content bg-purple">
+       <img src="@/assets/img413.png" class="image">
+      </div></el-col>
+  <el-col :span="12"><div class="grid-content bg-purple-light">
+      
+        <el-card>
+    <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="date1"
+      label="类别"
+     >
+    </el-table-column>
+    <el-table-column
+      prop="name1"
+      label="数量"
+      >
+    </el-table-column>
+     <el-table-column
+      prop="date2"
+      label="类别"
+     >
+    </el-table-column>
+    <el-table-column
+      prop="name2"
+      label="数量"
+     >
+    </el-table-column>
+     <el-table-column
+      prop="date3"
+      label="类别"
+      >
+    </el-table-column>
+    <el-table-column
+      prop="name3"
+      label="数量"
+      >
+    </el-table-column>
+  </el-table>
+  </el-card>
+      
+      
+      
+      
+      </div></el-col>
+</el-row>
+     
+    </el-card>
+
+    <br><br>
+  <h3>文化产业链</h3>
+<span>目前，环同济知识经济圈中集聚的企业以建筑与城市规划设计为龙头，辅以景观设计、环保工程设计、工程咨询、汽车设计、软件设计、艺术传媒设计、教育培训等知识服务业。这些企业中有2.4万余人的高素质就业人口，形成了中国最大的设计产业集群。</span>
+<span>同济大学以学科链嵌入产业链实现全面功能对接，积极形成紧密的产学研战略联盟。</span>
+
+
 </el-card>
+
+
+
 </div>
-
-
 </template>
 
 <script>
@@ -60,8 +110,68 @@ export default {
   data () {
     return {
       //
+      tableData: [{
+          date1: '会展、布展',
+          name1: '2',
+          date2: '商标事务所',
+          name2: '1',
+          date3: '书刊销售',
+          name3: '12',
+        }, {
+          date1: '书店',
+          name1: '23',
+          date2: '新华书店',
+          name2: '3',
+          date3: '科研机构',
+          name3: '66',
+        }, {
+          date1: '书店',
+          name1: '23',
+          date2: '新华书店',
+          name2: '3',
+          date3: '科研机构',
+          name3: '66',
+          
+        }, {
+          date1: '设计机构',
+          name1: '9',
+          date2: '设计院&事务所',
+          name2: '142',
+          date3: '监测检测、认证',
+          name3: '3',
+        },
+         {
+          date1: '高新技术',
+          name1: '131',
+          date2: '文化艺术团体',
+          name2: '18',
+          date3: '图书馆、博物馆',
+          name3: '4',
+         },
+          {
+          date1: '艺术展览',
+          name1: '2',
+          date2: '档案馆',
+          name2: '1',
+          date3: '文化活动场所',
+          name3: '16',
+         },
+        
+
+        ]
     }
-  }
+  },
+   methods: {
+    jumpto()
+    {
+     window.location.href ="https://blog.csdn.net/gang456789/article/details/78115589"
+     console.log("hhhkkj")
+    },
+     linkDownload (url) {
+      window.open(url,'_blank') // 新窗口打开外链接
+    }
+      
+    }
 }
 </script>
 
@@ -71,3 +181,6 @@ export default {
     width: 100%;
     display: block;
   }
+  .a-style {
+  text-decoration:none
+}
--- gisproject/src/components/page432.vue
+++ gisproject/src/components/page432.vue
@@ -1,99 +1,80 @@
 <template>
-    <div class="pageecoCircle container active">
-        <el-card>
-            <h2>环同济经济圈</h2>
-            <span>环同济经济圈，即“环同济研发设计服务特色产业基地”，重点发展创意和设计产业，国际工程咨询服务业，新能源新材料和环保科技产业三大密集群。</span>
-            <el-divider></el-divider>
-            <h2>核心区</h2>
-            <span>该产业基地的“核心圈”以同济大学四平路校区为核心，包括密云路、中山北二路、江浦路、控江路、大连路围合组成的区域，面积约2.6平方公里。</span>
-            <el-divider></el-divider>
-            <h2>扩展区</h2>
-            <span>环同济经济圈扩展区以曲阳路、大连西路-大连路、周家嘴路、黄兴路、邯郸路围合组成，面积约 10 平方公里。</span>
-            <span>在此基础上进一步向外扩展，形成四个“辐射点”，包括新江湾城辐射点、森林公园辐射点、黄兴公园辐射点、滨江辐射点。</span>
-            <br><br>
-            <div align="center">
-                <el-button type="primary" icon="el-icon-search">
-                  <a  class="a-style" @click="linkDownload('http://www.arcgis.com/home/webmap/viewer.html?url=https%3A%2F%2Flocalhost%3A6443%2Farcgis%2Frest%2Fservices%2F%25E7%258E%25AF%2FMapServer&source=sd')">查看地图
+<div class="pageecoCircle container active">
+<el-card>
+    <h2>环同济经济圈创业园区选址</h2>
+    <span>结合环同济经济圈附近区域的房价和交通情况，分析经济圈中哪些区域适合作为新的文化创业产业园进行开发。</span>
+    <el-divider></el-divider>
+    <h3>房价</h3>
+    <span>从房价网爬取50个写字楼的位置和租金，并使数据点均匀分布在杨浦区的各个区域：</span>
+    <el-divider></el-divider>
+    <h3>交通情况</h3>
+    <el-divider></el-divider>
+    <h3>建立模型</h3>
+    <el-divider></el-divider>
+    <h3>最终结论</h3>
+    <div align="center">
+    <el-button type="primary" icon="el-icon-search">
+      <a  class="a-style" @click="linkDownload('http://www.arcgis.com/home/webmap/viewer.html?url=https%3A%2F%2Flocalhost%3A6443%2Farcgis%2Frest%2Fservices%2F%25E7%258E%25AF%2FMapServer&source=sd')">查看地图
 
-                  </a>
-                </el-button>
-            </div>
-            <br><br>
-            <el-divider></el-divider>
-            <div id="viewDivEcoCircle"></div>
-            <span>环同济经济圈文化产业数量为522，环同济经济扩展区文化产业数量为861，环同济经济圈辐射点共有2277家文化产业。</span>
-            <span>杨浦区的文化产业数量为3317，环同济经济圈文化产业数量为522，因此环同济经济圈内的文化产业占杨浦区文化产业比例为15.737%</span>
-
-
-            <span>至“十一五”末，杨浦区文化创意产业共有企业3237家，从业人员5.48万人；实现总产出199.61亿元，同比增长17.9%，与2004年（经济普查年）相比，年均增长18.9%，占全市比重5%；实现增加值64.88亿元，同比增长15.7%，占杨浦生产总值的比重为7.3%（扣除烟草业后占比13.5%），与上年基本持平，占全市文化创意产业比重为3.9%。环同济经济圈的文化产业的经济规模占杨浦区文化产业经济规模75.147%。</span>
-            <div id="mainDivEcoCircle" style="width: 100%;height:500px;"></div>
-            <span>下图所示的根据三次经济普查数据“环同济”产业结构的变化</span>
-            <div id="industryStruc" style="width: 100%;height:500px;"></div>
-            <span>因此参照《国民经济行业分类》目录，结合该区域的产业发展态势，可以把环同济知识经济圈文化创意产业划分为九大类。这九大类分别是：文化艺术类，信息服务类，产品设计类，建筑景观设计类，咨询策划类，文化用品、设备及相关文化产品的生产类，文化用品、设备及相关文化产品的销售类，教育、卫生、体育事业类，以及其他类。</span>
-            <span>其中重点发展的产业有四类（有先后次序）:</span>
-            <span>1.建筑景观设计类，具体包括建筑设计业、城市规划设计业、室内设计业、城市绿化设计业。</span>
-            <span>2.产品设计类，重点是工业设计业</span>
-            <span>3.文化艺术类，包括文化艺术活动业、出版业等。</span>
-            <span style="margin-bottom: 70px;">4.信息服务类，重点是互联网信息服务业。</span>
-        </el-card>
-    </div>
+      </a>
+    </el-button>
+</div>
+    
+</el-card>
+</div>
 
 
 </template>
 
 <script>
 
-    export default {
+export default {
+ 
+  components: {
 
-        components: {},
-        data() {
-            return {
-                //
-            }
-        },
-        methods: {
-            jumpto() {
-                window.location.href = " http://www.arcgis.com/home/webmap/viewer.html?url=https%3A%2F%2Flocalhost%3A6443%2Farcgis%2Frest%2Fservices%2F%25E7%258E%25AF%2FMapServer&source=sd"
-
-            },
-            linkDownload (url) {
-                window.open(url,'_blank') // 新窗口打开外链接
-            },
-        }
+  },
+  data () {
+    return {
+      //
+      linkDownload (url) {
+    window.open(url,'_blank') // 新窗口打开外链接
+},
     }
+  }
+}
 </script>
 
 <style>
-    .a-style {
-        text-decoration:none
-    }
-    .time {
-        font-size: 13px;
-        color: #999;
-    }
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
 
-    .bottom {
-        margin-top: 13px;
-        line-height: 12px;
-    }
+  .button {
+    padding: 0;
+    float: right;
+  }
 
-    .button {
-        padding: 0;
-        float: right;
-    }
+  .image {
+    width: 100%;
+    display: block;
+  }
 
-    .image {
-        width: 100%;
-        display: block;
-    }
-
-    .clearfix:before,
-    .clearfix:after {
-        display: table;
-        content: "";
-    }
-
-    .clearfix:after {
-        clear: both
-    }
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
+   .a-style {
+  text-decoration:none
+}
 </style>
